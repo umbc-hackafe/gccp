d2d940aa5b654b3bd514fc13b4488a43fec92f56
Zachariah T. Dzielinski <zachdzielinski@gmail.com>
2016-03-27 01:12:15 -0400

    Sun Mar 27 01:12:15 EDT 2016

--- tools/quick.sh
+++ tools/quick.sh
@@ -4,17 +4,16 @@
 # $2 - File to write to
 
 # No arguments given
-if [ $# -eq 0 ]
-  then
+if [ $# -eq 0 ]; then
     # Print usage message
     /usr/bin/echo "usage: ./quick.sh <data> <file>"
+  else
+    # Hard write data to file
+    /usr/bin/echo $1 > $2
+    # Add file that was written
+    /usr/bin/git add --all
+    # Commit with datetime as message
+    /usr/bin/git commit -m "`/usr/bin/date`"
+    # Push changes
+    /usr/bin/git push
 fi
-
-# Hard write data to file
-/usr/bin/echo $1 > $2
-# Add file that was written
-/usr/bin/git add --all
-# Commit with datetime as message
-/usr/bin/git commit -m "`/usr/bin/date`"
-# Push changes
-/usr/bin/git push
